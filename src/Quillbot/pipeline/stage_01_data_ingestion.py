4b323e038919bd07d5a24f6e6d805c5984db24ab
sherwin-huynh <huynhchitrung020503@gmail.com>
2025-04-21 20:02:13 +0700

    Data ingestion

---
+++ src/Quillbot/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,15 @@
+from Quillbot.config.configuration import ConfigurationManager
+from Quillbot.conponents.data_ingestion import DataIngestion
+from Quillbot.logging import logger
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
