7aaa8d562ccade4dc9a947c7f96f8d2f351c8c69
sherwin-huynh <huynhchitrung020503@gmail.com>
2025-05-05 20:24:10 +0700

    Data validation + model_trainer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Quillbot.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Quillbot.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from Quillbot.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-# from Quillbot.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from Quillbot.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # from Quillbot.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from Quillbot.logging import logger
 
@@ -43,16 +43,16 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Model Trainer stage"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    model_trainer = ModelTrainerTrainingPipeline()
-#    model_trainer.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
